eadc631b5e8184b19256495f4a872b172b6d90d4
2Bartek2 <ostrowskibartosz85@gmail.com>
2023-03-25 13:29:44 +0100

    Create auth module, user entity, and signUp method( hash and salt password, error handling)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,37 +1,39 @@
-import {ConflictException, Injectable, InternalServerErrorException} from '@nestjs/common';
-import {InjectRepository} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
-import {User} from "./user.entity";
-import {AuthCredentialsDto} from "./dto/auth-credentials.dto";
-import * as bcrypt from 'bcrypt'
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>
-    ) {}
-    async signUp(authCredentialsDto:AuthCredentialsDto) : Promise<void> {
-        const {username, password} = authCredentialsDto
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
 
+    const user = new User();
+    user.username = username;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
-        const user = new User()
-        user.username = username
-        user.salt = await bcrypt.genSalt()
-        user.password = await this.hashPassword(password, user.salt)
-
-        try {
-            await user.save()
-        } catch (error) {
-            if (error.code === '23505') {
-                throw new ConflictException('Username already exist')
-            } else {
-                throw new InternalServerErrorException()
-            }
-
-        }
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exist');
+      } else {
+        throw new InternalServerErrorException();
+      }
     }
-    private async hashPassword ( password : string, salt: string) : Promise<string>{
-        return bcrypt.hash(password,salt)
-    }
+  }
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
 }
