0e5343ce7e0cb9f9dd12ccf882ffd198a6c12937
Tomasz Dziedzio <t.dziedzio@futuremind.com>
2023-04-02 09:08:40 +0200

    add pass unhash

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -32,6 +34,27 @@ export class AuthService {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.userRepository.findOne({ where: { username } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    //const unhashPass = bcrypt.compare(pass, user.password)
+    const result1 = await this.hashPassword(pass, user.salt);
+    if (result1 === user.password) {
+      return 'huju zalogowales sie';
+    } else {
+      throw new UnauthorizedException('cos zle wpisales huju');
+    }
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return result;
   }
   private async hashPassword(password: string, salt: string): Promise<string> {
     return bcrypt.hash(password, salt);
